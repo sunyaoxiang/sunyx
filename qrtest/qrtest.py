ca6fb7779e4b45024da07d32a2e0e8798984d1b1
yaoxiang.sun <yaoxiang.sun@datayes.com>
2017-11-09 19:51:23 +0800

    1.somethins for nothing

--- qrtest/qrtest.py
+++ qrtest/qrtest.py
@@ -2,9 +2,21 @@
 import qrcode
 
 
-qr = qrcode.QRCode(version= None)
+class make_png(object):
 
-qr.add_data("yes man")
-qr.make(fit=True)
-img = qr.make_image()
-img.save("see.png")
+    def __init__(self,url):
+        self.url = url
+        # print url
+
+    def get_png(self,name):
+        qr = qrcode.QRCode(version= None)
+        qr.add_data(self.url)
+        qr.make(fit=True)
+        img = qr.make_image()
+        img.save("{}.png".format(name))
+
+url = "https://github.com/rainyear/pytips/blob/master/Markdowns/{}.md".format("2016-05-11-Floating-Point-Arithmetic")
+a = make_png(url).get_png("abc")
+b = make_png(url).get_png
+b("cash")
+pass
